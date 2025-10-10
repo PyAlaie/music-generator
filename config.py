acb2f816326f426c77c12a0858a191e97794ee9d
MohammadHossein Bagheri <py.persian@gmail.com>
2025-10-11 01:47:53 +0330

    implement concurrent downloads; minor cleanup

--- config.py
+++ config.py
@@ -1,20 +1,22 @@
 import os
+import pathlib
 
-_current_file = os.path.abspath(__file__)
-BASEDIR = os.path.dirname(_current_file)
+BASE_DIR = pathlib.Path(__file__).parent
+DATA_PATH = BASE_DIR / 'data'
 
-DATA_PATH = BASEDIR + '/data'
 
 class Crawl:
     base_url = 'https://www.midiworld.com/'
     composers_url = 'composers.htm'
 
+
 class MidiFiles:
-    database = DATA_PATH + '/db.csv'
-    raw_midi_files = DATA_PATH + '/midi'
-    encoded_files = DATA_PATH + '/encoded'
-    preprocessed_csv_files = DATA_PATH + '/preprocessed'
-    temp_space = DATA_PATH + '/temp'
-    log_space = DATA_PATH + '/log'
-    weights_path = DATA_PATH + '/models'
-    generated_csv_path = DATA_PATH + '/generated_csv'
+    database = DATA_PATH / 'db.csv'
+    raw_midi_files = DATA_PATH / 'midi'
+    encoded_files = DATA_PATH / 'encoded'
+    preprocessed_csv_files = DATA_PATH / 'preprocessed'
+    temp_space = DATA_PATH / 'temp'
+    log_space = DATA_PATH / 'log'
+    weights_path = DATA_PATH / 'models'
+    generated_csv_path = DATA_PATH / 'generated_csv'
+
--- src/crawl.py
+++ src/crawl.py
@@ -1,23 +1,61 @@
+import asyncio
+import pathlib
+import re
+import random
+import os
+import time
+
 import requests
 from bs4 import BeautifulSoup 
-import re, config, tqdm, random, pandas as pd, os, time
+from tqdm.asyncio import tqdm
+import pandas as pd
+import aiohttp
+
+import config
+
+
+STREAM_CHUNK_SIZE = 4096
+
+
+async def batch_download(info, callback=None, max_conn=32, progress=True, progress_settings={}):
+    q = asyncio.Queue()
+    if progress:
+        pg = tqdm(total=len(info), **progress_settings)
+
+    # TODO: handle task cancellation.
+    async def worker():
+        while True:
+            url, path, *args = await q.get()
+
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    with open(path, 'wb') as f:
+                        async for chunk in response.content.iter_chunked(STREAM_CHUNK_SIZE):
+                            f.write(chunk)
+                    if progress:
+                        pg.update(1)
+                    if callback:
+                        await callback(*args)
+            q.task_done()
+        
+    tasks = [asyncio.create_task(worker()) for _ in range(max_conn)]
+    for download_info in info:
+        await q.put(download_info)
+    await q.join()
+    for t in tasks:
+        t.cancel()
+    
 
 def generate_random_string(length=16):
-    chars = [chr(i) for i in range(97,123)]
-    chars = ''.join(chars)
-    chars += chars.upper()
-    numbers = [str(i) for i in range(0,10)]
-    numbers = ''.join(numbers)
+    chars = [chr(i) for i in range(ord('a'), ord('z')+1)]
+    chars = [chr(i) for i in range(ord('A'), ord('Z')+1)]
+    chars.extend([str(i) for i in range(10)])
+    return ''.join(random.choices(chars, k=length))
 
-    chars += numbers
-
-    random_string = [random.choice(chars) for i in range(length)]
-    
-    return ''.join(random_string)
 
 def extract_composers_urls():
     composers = requests.get(config.Crawl.base_url + config.Crawl.composers_url)
-    soup = BeautifulSoup(composers.text, "html.parser")
+    soup = BeautifulSoup(composers.text, 'html.parser')
 
     blockquote = soup.find_all(name='blockquote')
     blockquote = blockquote[1]
@@ -32,74 +70,86 @@ def extract_composers_urls():
 
     return composers_dict
 
+
+def extract_composer_tracks(composer_page_url):
+    page = requests.get(composer_page_url)
+
+    page_soup = BeautifulSoup(page.text, 'html.parser')
+
+    pattern = r'^https://www\.midiworld\.com/.+\.mid$'
+    pattern = re.compile(pattern)
+
+    p = page_soup.find_all('a', href=pattern)
+
+    tracks = []
+    for i in p:
+        track_name = i.text.strip().replace(' ', '_')
+        url = i.get('href')
+        tracks.append((track_name, url))
+
+    return tracks
+
+
 def read_db(path=config.MidiFiles.database):
     if not os.path.exists(path) or os.path.getsize(path) == 0:
-        headers = ["Composer", "Name", "FileName", "Url"]
+        headers = ['Composer', 'Name', 'FileName', 'Url']
 
         df = pd.DataFrame(columns=headers)
+        pathlib.Path(path).parent.mkdir(parents=True)
         df.to_csv(path, index=False)
 
     db = pd.read_csv(path)
     return db
 
+
 def download_composers_midi(composers_dict, no_redownload=True):
     db = read_db()
+    pathlib.Path(config.MidiFiles.raw_midi_files).mkdir(parents=True, exist_ok=True)
+   
+    async def _download_callback(row):
+        nonlocal db
+        db = pd.concat([db, pd.DataFrame([row])], ignore_index=True)
 
     try:
         for composer_name, link in composers_dict.items():
-            print(f"Download {composer_name} Files")
+            print(f'Download {composer_name} Files')
 
-            page = requests.get(config.Crawl.base_url + link)
+            download_info = []
+            composer_tracks = extract_composer_tracks(config.Crawl.base_url + link)
 
-            page_soup = BeautifulSoup(page.text, "html.parser")
+            for track in composer_tracks:
+                track_name, track_url = track
 
-            pattern = r'^https://www\.midiworld\.com/.+\.mid$'
-            pattern = re.compile(pattern)
-
-            p = page_soup.find_all('a', href=pattern)
-
-            for i in tqdm.tqdm(p, desc="Downloading MIDI files", unit="file"):
-                music_name = i.text.strip().replace(' ', '_') 
-                
-                filename = [composer_name, music_name, generate_random_string(5)]
-                filename = '_'.join(filename)
+                filename = f'{composer_name}_{track_name}_{generate_random_string(5)}.mid'
                 filename = re.sub(r'[\/\\:*?"<>|\n\r\t]', '_', filename)
-                filename += '.mid'
-                url = i.get("href")
                 
                 # Check if we already have downloaded the file
-                if no_redownload and url in db['Url'].values:
+                if no_redownload and track_url in db['Url'].values:
                     continue
 
-                response = requests.get(url, stream=True)
-        
-                if response.status_code == 200:
-                    with open(config.MidiFiles.raw_midi_files + '/' + filename, "wb") as f:
-                        for chunk in response.iter_content(chunk_size=8192):
-                            if chunk:
-                                f.write(chunk)
-                
-                    row = {
-                        "Composer": composer_name,
-                        "Name": music_name,
-                        "FileName": filename, 
-                        "Url": url
-                    }
+                row = {
+                    'Composer': composer_name,
+                    'Name': track_name,
+                    'FileName': filename, 
+                    'Url': track_url
+                }
 
-                    db = pd.concat([db, pd.DataFrame([row])], ignore_index=True)
-                
+                download_info.append((track_url, config.MidiFiles.raw_midi_files / filename, row))
+
+            asyncio.run(
+                batch_download(
+                    download_info,
+                    _download_callback, 
+                    progress_settings={'desc': 'Downloading MIDI Files'}
+                )
+            )
             db.to_csv(config.MidiFiles.database, index=False)
-        
-    except KeyboardInterrupt:
+    finally:
         db.to_csv(config.MidiFiles.database, index=False)
-        print("Saved the progress!")
-
-    except Exception as e:
-        print(e)
-        db.to_csv(config.MidiFiles.database, index=False)
-        print("Saved the progress!")
+        print('Saved the progress!')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     composers = extract_composers_urls()
     download_composers_midi(composers)
+
